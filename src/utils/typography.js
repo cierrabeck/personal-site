81e3c5307a2d7e81b8973f31f5c262517f1b3df4
cierrabeck <cnb44@cornell.edu>
2020-07-28 17:00:09 -0400

    add fonts and additional pages

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+import elkGlenTheme from 'typography-theme-elk-glen'
+
+const typography = new Typography(elkGlenTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
