595cc2433cff225961220bf5db1f3f6a0a072e33
cierrabeck <cnb44@cornell.edu>
2020-07-29 12:12:08 -0400

    add theme src files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Container from "../components/container"
+import Container from "components/common/container"
 
 export default function About() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
-import Layout from "../components/layout"
+import React from 'react';
+import { Layout, SEO } from 'components/common';
+import { Intro, Skills, Contact, Projects } from 'components/landing';
 
-export default function Home() {
-  return (
-    <Layout>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </Layout>
-  )
-}
+export default () => (
+  <Layout>
+    <SEO />
+    <Intro />
+    <Projects />
+    <Skills />
+    <Contact />
+  </Layout>
+);
